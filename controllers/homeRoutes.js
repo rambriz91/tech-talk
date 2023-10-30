86a8b0a97175b3a7985fac87db85ec1bdecb258a
rambriz91 <rambriz91@gmail.com>
2023-10-30 08:53:11 -0700

    added post.handlebars.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const withAuth = require('../utils/auth');
 const { Post, User } = require('../models');
 
-//Prevents unauthenticated users from accessing the homepage.
+//homepage displays all tech blog posts and user info.
 router.get('/', withAuth, async (req, res) => {
   try {
     const postData = await Post.findAll({
@@ -16,6 +16,31 @@ router.get('/', withAuth, async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render('homepage', {
       posts,
+      user: req.session.user,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//route for individual post
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+    const posterName = post.user.name;
+
+    res.render('post', {
+      ...post,
+      posterName,
       user: req.session.user,
       logged_in: req.session.logged_in,
     });
